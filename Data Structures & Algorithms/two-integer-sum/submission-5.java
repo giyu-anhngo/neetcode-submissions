419364350fcef21921418acd4c3c84ea46cca530
Huy Anh Ngo <54490272+giyu-anhngo@users.noreply.github.com>
2026-05-10 11:35:14 +0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        /*
+        Approach 02: Hash Table
+        Explaination:
+            * Searches for the current value's complement in the map.
+            * If not found, stores the current value and its index for future lookups.
+        TC: O(n) | SC: O(n)
+        */
+
+        Map<Integer, Integer> complementToIndexs = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (complementToIndexs.containsKey(complement)) {
+                return new int[] {complementToIndexs.get(complement), i};
+            }
+
+            complementToIndexs.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
