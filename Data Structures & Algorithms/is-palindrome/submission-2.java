ea5eeae2dc2ef4f4b1c4e363877c7a04b53be15f
Huy Anh Ngo <54490272+giyu-anhngo@users.noreply.github.com>
2026-05-16 08:37:56 +0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // TC: O(N)
+        // SC: O(N)
+
+        String normalized = normalize(s);
+        int left = 0;
+        int right = normalized.length() - 1;
+
+        while (left < right) {
+            if (normalized.charAt(left) != normalized.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private String normalize(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c: s.toCharArray()) {
+            if (isAlphanumeric(c)) {
+                stringBuilder.append(Character.toLowerCase(c));
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'a' && c <= 'z')
+            || (c >= 'A' && c <= 'Z')
+            || (c >= '0' && c <= '9');
+    }
+}
