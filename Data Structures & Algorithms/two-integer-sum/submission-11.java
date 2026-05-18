2d53af4ffaa8b4d61c0cd8e13a6dc825b7b79272
Huy Anh Ngo <54490272+giyu-anhngo@users.noreply.github.com>
2026-05-18 14:15:44 +0700

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // TC: O(n)
+        // SC: O(n)
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (numToIndex.containsKey(complement)) {
+                return new int[] {numToIndex.get(complement), i};
+            }
+
+            numToIndex.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
